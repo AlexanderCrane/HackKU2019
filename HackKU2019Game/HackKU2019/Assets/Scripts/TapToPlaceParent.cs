280f3ce51cd5fd81aa20ced3a151660b209ce92e
AlexanderCrane <InnovateTheWorld@yahoo.com>
2019-02-09 16:42:05 -0600

    idk

--- HackKU2019Game/HackKU2019/Assets/Scripts/TapToPlaceParent.cs
+++ HackKU2019Game/HackKU2019/Assets/Scripts/TapToPlaceParent.cs
@@ -4,6 +4,24 @@ public class TapToPlaceParent : MonoBehaviour
 {
     bool placing = false;
 
+    private void Start()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(this.transform.parent.position, new Vector3(this.transform.parent.position.x, this.transform.parent.position.y - 100f, this.transform.parent.position.z), out hitInfo, 100.0f, SpatialMapping.PhysicsRaycastMask))
+        {
+            // Move this object's parent object to
+            // where the raycast hit the Spatial Mapping mesh.
+            this.transform.parent.position = hitInfo.point;
+
+            // Rotate this object's parent object to face the user.
+            Quaternion toQuat = Camera.main.transform.localRotation;
+            toQuat.x = 0;
+            toQuat.z = 0;
+            this.transform.parent.rotation = toQuat;
+        }
+
+    }
+
     // Called by GazeGestureManager when the user performs a Select gesture
     void OnSelect()
     {
